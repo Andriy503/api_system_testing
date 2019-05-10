4fb77ff7fb7adb730134655c19320df4fe6cffa8
sokol <devsokol555@izigroup.nyc>
2019-05-10 20:50:08 +0300

    add three type answers

--- src/Controller/AnswersController.php
+++ src/Controller/AnswersController.php
@@ -50,6 +50,19 @@ class AnswersController extends AppController
     public function addAnswer() {
         if ($this->request->is('POST')) {
             $params = $this->request->getData();
+            $idQuestion = $this->request->getData('id_question', false);
+
+            try {
+                $question = $this->Questions->get($idQuestion);
+            } catch (\Exception $e) {
+                return $this->Core->jsonResponse(false, 'Connection Error');
+            }
+
+            $checkRootAddAnswer = $this->_cheketRootAnswerType($question, $params);
+
+            if (!$checkRootAddAnswer['success']) {
+                return $this->Core->jsonResponse(false, $checkRootAddAnswer['message']);
+            }
 
             $answer = $this->Answers->newEntity($params);
 
@@ -107,5 +120,101 @@ class AnswersController extends AppController
                 'answer' => $editAnswer
             ]);
         }
+    }
+
+    private function _cheketRootAnswerType($question, $preAnswer) {
+        $associations = [
+            1 => '_oneAnswer',
+            2 => '_manyAnswers',
+            3 => '',
+            4 => '_wordAnswer'
+        ];
+
+        $result = call_user_func_array(
+            // name function
+            [
+                $this,
+                $associations[$question->id_type]
+            ],
+            // params
+            [
+                $question,
+                $preAnswer
+            ]
+        );
+
+        return $result;
+    }
+
+    private function _oneAnswer($question, $preAnswer) {
+        $answers = $this->Answers->find()
+            ->where([
+                'id_question' => $question->id
+            ])
+            ->toArray();
+
+        // check count answer
+        if (count($answers) >= 4) {
+            return [
+                'success' => false,
+                'message' => 'Питання не може містити більше ніж 4 відповіді!'
+            ];
+        }
+
+        // check current answer true
+        if ($preAnswer['current_answer']) {
+            $current_answers = array_column($answers, 'current_answer');
+
+            if (in_array(true, $current_answers)) {
+                return [
+                    'success' => false,
+                    'message' => 'Питання не може мати більше ніж 1 правильну відповідь!'
+                ];
+            }
+        }
+
+        return [
+            'success' => true
+        ];
+    }
+
+    private function _manyAnswers($question, $preAnswer) {
+        $answers = $this->Answers->find()
+            ->where([
+                'id_question' => $question->id
+            ])
+            ->toArray();
+
+        // check count answer
+        if (count($answers) >= 4) {
+            return [
+                'success' => false,
+                'message' => 'Питання не може містити більше ніж 4 відповіді!'
+            ];
+        }
+
+        return [
+            'success' => true
+        ];
+    }
+
+    private function _wordAnswer($question, $preAnswer) {
+        $answers = $this->Answers->find()
+            ->where([
+                'id_question' => $question->id
+            ])
+            ->toArray();
+
+        // check count answer
+        if (count($answers) >= 1) {
+            return [
+                'success' => false,
+                'message' => 'Питання не може містити більше ніж 1 відповідь!'
+            ];
+        }
+
+        return [
+            'success' => true
+        ];
     }
 }
